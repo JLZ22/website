b365f39a7d6bbb7e8d5eba1fb24d893ae5f7a83b
John Zeng <johnzeng878@gmail.com>
2024-12-18 03:18:23 -0800

    add collections for projects - each entry sourced from mdx file TODO: get tailwind to work with the mdx

---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+
+const project = defineCollection({
+  loader: glob({pattern: '*.mdx', base: './src/content/projects'}),
+  type: "content_layer",
+  schema: z.object({
+    title: z.string(),
+    dateRange: z.string(),
+    link: z.string(),
+  }),
+});
+
+export const collections = { project };
